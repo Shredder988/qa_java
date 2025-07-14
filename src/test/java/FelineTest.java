9cdd83ee9a680c7318610703e0e5fa8c7c45d252
User Kolevatov <username@yandex.ru>
2025-07-14 14:34:05 +0300

    Second_commit

---
+++ src/test/java/FelineTest.java
@@ -0,0 +1,28 @@
+import com.example.Feline;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FelineTest {
+    @Test
+    void eatMeat() throws Exception {
+        Feline feline = new Feline();
+        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        assertEquals(expectedFood, feline.eatMeat());
+    }
+
+    @Test
+    void getFamily() {
+        Feline feline = new Feline();
+        assertEquals("Кошачьи", feline.getFamily());
+    }
+
+    @Test
+    void getKittens() {
+        Feline feline = new Feline();
+        int expectedKittensCount = 1;
+        assertEquals(expectedKittensCount, feline.getKittens(expectedKittensCount));
+    }
+}
